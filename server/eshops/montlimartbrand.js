86f8aabf28b3468aaa31ce1bc45972debaf47ef4
Arnaud COURNIL <24459@student.dorset-college.ie>
2023-02-16 10:45:49 +0100

    Generate products.json in server

---
+++ server/eshops/montlimartbrand.js
@@ -0,0 +1,94 @@
+function uuidv4() { // Public Domain/MIT
+    var d = new Date().getTime();//Timestamp
+    var d2 = ((typeof performance !== 'undefined') && performance.now && (performance.now()*1000)) || 0;//Time in microseconds since page-load or 0 if unsupported
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16;//random number between 0 and 16
+        if(d > 0){//Use timestamp until depleted
+            r = (d + r)%16 | 0;
+            d = Math.floor(d/16);
+        } else {//Use microseconds since page-load if supported
+            r = (d2 + r)%16 | 0;
+            d2 = Math.floor(d2/16);
+        }
+        return (c === 'x' ? r : (r & 0x3 | 0x8)).toString(16);
+    });
+  }
+
+const fetch = require('node-fetch');
+const cheerio = require('cheerio');
+
+/**
+ * Parse webpage e-shop
+ * @param  {String} data - html response
+ * @return {Array} products
+ */
+const parse = async data => {
+  const $ = cheerio.load(data);
+
+  return $('div.products-list__block')
+    .map(async(i, element) => {
+      const link = $(element)
+        .find('.product-miniature__thumb-link')
+        .attr('href');
+      var name = $(element)
+        .find('.product-miniature__title .text-reset')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ') +
+        " " +
+        $(element)
+        .find('.product-miniature__color')
+        .text()
+        .trim()
+        .replace(/\s/g, ' ');
+      name = name.trim();
+      var image = "";
+      if($(element).find('video').length > 0) {
+        await fetch(link).then(async response => {
+          await response.text().then(async body => {
+            const $b = cheerio.load(body);
+            image = $b('img')[0].attribs['data-src'];
+          })
+        })
+      }
+      else {
+        image = $(element)
+          .find('.product-miniature__thumb img')[0].attribs['data-src'];
+      }
+      const price = parseFloat(
+        $(element)
+          .find('.price')
+          .text()
+      );
+      const scrapDate = new Date().toDateString();
+      const brand = "Montlimart";
+      const uuid = uuidv4();
+      return {image, link, name, price, scrapDate, brand, uuid};
+    })
+    .get();
+};
+
+/**
+ * Scrape all the products for a given url page
+ * @param  {[type]}  url
+ * @return {Array|null}
+ */
+
+module.exports.scrape = async url => {
+  try {
+    const response = await fetch(url);
+
+    if (response.ok) {
+      const body = await response.text();
+
+      return await Promise.all(await parse(body))
+    }
+
+    console.error(response);
+
+    return null;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
--- server/sandbox.js
+++ server/sandbox.js
@@ -1,18 +1,64 @@
 /* eslint-disable no-console, no-process-exit */
 const dedicatedbrand = require('./eshops/dedicatedbrand');
+const montlimartbrand = require('./eshops/montlimartbrand');
+const circlesportswear = require('./eshops/circlesportswearbrand');
 
-async function sandbox (eshop = 'https://www.dedicatedbrand.com/en/men/news') {
-  try {
-    console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+const link = [
+  "https://shop.circlesportswear.com/collections/all", //tout le site
+  "https://www.montlimart.com/99-vetements", //tout le site
+  "https://www.montlimart.com/14-chaussures",
+  "https://www.montlimart.com/15-accessoires",
+  "dedicatedbrand"//tout le site
+]
 
-    const products = await dedicatedbrand.scrape(eshop);
-
-    console.log(products);
-    console.log('done');
+async function sandbox (eshop = undefined, number = -1) {
+  if(number == -1 && eshop == undefined)
+  {
+    var allProducts = [];
+    for(var i = 0; i < link.length; i++)
+    {
+      allProducts.push(...await sandbox(link[i], i));
+    }
+    const fs = require('fs');
+ 
+    let data = JSON.stringify(allProducts);
+    fs.writeFileSync('products.json', data);
+    console.log("Products in the json file: " + allProducts.length);
     process.exit(0);
-  } catch (e) {
-    console.error(e);
-    process.exit(1);
+  }
+  else
+  {
+    try {
+      console.log(`🕵️‍♀️  browsing ${eshop} eshop`);
+      var products = "";
+      if(eshop.includes('montlimart')){
+        products = await montlimartbrand.scrape(eshop);
+      }
+      else if(eshop == 'dedicatedbrand'){
+        products = await dedicatedbrand.getProducts();
+      }
+      else if(eshop.includes('dedicatedbrand')){//only fourty first products
+        products = await dedicatedbrand.scrape(eshop);
+      }
+      else if(eshop.includes('circlesportswear')){
+        products = await circlesportswear.scrape(eshop);
+      }
+      else
+      {
+        console.log('eshop not found');
+        process.exit(1);
+      }
+      console.log(products);
+      console.log('done ' + products.length + ' products found');
+      if(number == -1)
+      {
+        process.exit(0);
+      }
+      return products;
+    } catch (e) {
+      console.error(e);
+      process.exit(1);
+    }
   }
 }
 
