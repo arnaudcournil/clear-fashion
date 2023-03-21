d274e9fc4948f1e8efbef9f804b8f731f4381304
Arnaud COURNIL <24459@student.dorset-college.ie>
2023-03-21 18:37:57 +0100

    vercel update

--- server/api.js
+++ server/api.js
@@ -21,22 +21,12 @@ app.get('/', (request, response) => {
 
 app.get('/products/search', async (request, response) => {
   console.log("Requete : /products/search, params : ", request.query);
-  try{
-    var brand = request.query.brand;
-  }catch(e){
-    var brand = undefined;
-  };
-  try{
-  var lessThan = parseFloat(request.query.price);
-  }catch(e){
-    var lessThan = -1;
-  };
-  try{
-  var limit = request.query.limit;
-  }catch(e){
-    var limit = "none";
-  };
-
+  //var brand = request.query.brand;
+  //var lessThan = parseFloat(request.query.price);
+  //var limit = request.query.limit;
+var brand = undefined;
+var lessThan = -1;
+var limit = "none";
   var products = await MongoClient.fetchProducts(brand, lessThan);
 
   var result = limit === "none" ? products.slice(0, limit) : products;
