8dc2f0ad97b8d0f561130e5a160f84bf7622f3fc
Arnaud COURNIL <24459@student.dorset-college.ie>
2023-03-21 18:17:59 +0100

    vercel update

--- server/api.js
+++ server/api.js
@@ -21,7 +21,7 @@ app.get('/', (request, response) => {
 
 app.get('/products/search', async (request, response) => {
   console.log("Requete : /products/search, params : ", request.query);
-  response.send(request.query);
+  response.send("ok");
   var brand = request.query.brand;
   var lessThan = parseFloat(request.query.price);
   var limit = request.query.limit;
