6ad4d3b24cc52e9c51a82daa24e6b4cd28174a39
Arnaud COURNIL <24459@student.dorset-college.ie>
2023-03-22 11:58:00 +0100

    adapt api to client

--- server/api.js
+++ server/api.js
@@ -17,6 +17,9 @@ app.options('*', cors());
 
 app.get('', async(request, response) => {
   console.log("Requete : /products/search, params : ", request.query);
+  var body = {}
+  body.success = true;
+
   var brand = request.query.brand;
   if(!brand) var brand = null;
   if(request.query.price)var lessThan = parseFloat(request.query.price);
@@ -27,11 +30,16 @@ app.get('', async(request, response) => {
   var result = products;
   var result = limit !== null ? products.slice(0, limit) : products;
 
-  response.send(result);
+  body.data.result = result;
+
+  response.send(body);
 });
 
 app.get('/products/search', async (request, response) => {
   console.log("Requete : /products/search, params : ", request.query);
+  var body = {}
+  body.success = true;
+
   var brand = request.query.brand;
   if(!brand) var brand = null;
   if(request.query.price)var lessThan = parseFloat(request.query.price);
@@ -42,25 +50,33 @@ app.get('/products/search', async (request, response) => {
   var result = products;
   var result = limit !== null ? products.slice(0, limit) : products;
 
-  response.send(result);
+  body.data.result = result;
+
+  response.send(body);
 });
 
 app.get('/products/*', async (request, response) => {
   console.log("Requete : products/:id, params : ", request.params[0]);
+  var body = {}
+  body.success = true;
 
   var id = request.params[0];
 
   var product = await MongoClient.fetchProductsByUuid(id);
 
-  response.send(product);
+  body.data.result = product;
+  response.send(body);
 });
 
 app.get('/brands', async (request, response) => {
   console.log("Requete : /brands, params : ", request.query);
+  var body = {}
+  body.success = true;
 
   var brands = await MongoClient.getBrands();
 
-  response.send(brands);
+  body.data.result = brands;
+  response.send(body);
 });
 
 app.listen(PORT);
